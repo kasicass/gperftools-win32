a8c3f6470dd9abc522856ca2d08b969e51d50085
Kasicass Tang <kasicass@gmail.com>
2013-12-23 16:37:22 +0800

    make profile runs

--- tests/libtcmalloc-static-test/main.cpp
+++ tests/libtcmalloc-static-test/main.cpp
@@ -1,12 +1,19 @@
 #include <Windows.h>
 #include <stdio.h>
 
+extern "C" void HeapProfilerStart(const char* prefix);
+extern "C" void HeapProfilerDump(const char *reason);
+
 int main()
 {
+	HeapProfilerStart("D:\\heap");
+
 	printf("begin\n");
 	char *p = new char[1024];
-	Sleep(1000*10);
+	//Sleep(1000*10);
+	HeapProfilerDump("Try1");
 	delete[] p;
 	printf("end\n");
+
 	return 0;
 }
