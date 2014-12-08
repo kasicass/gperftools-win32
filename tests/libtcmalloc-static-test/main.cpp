c5605c969e6fb24f5464de5755c697a11ebd836b
kasicass <kasicass@gmail.com>
2014-12-08 11:14:10 +0800

    more test

--- tests/libtcmalloc-static-test/main.cpp
+++ tests/libtcmalloc-static-test/main.cpp
@@ -4,16 +4,36 @@
 extern "C" void HeapProfilerStart(const char* prefix);
 extern "C" void HeapProfilerDump(const char *reason);
 
+void *mallocMe(unsigned int n)
+{
+	return malloc(n);
+}
+
 int main()
 {
 	HeapProfilerStart("D:\\heap");
 
-	printf("begin\n");
+	//printf("begin\n");
 	char *p = new char[1024];
 	//Sleep(1000*10);
 	HeapProfilerDump("Try1");
 	delete[] p;
-	printf("end\n");
+	//printf("end\n");
+
+	const int N = 3;
+	void *pv[N] = {0};
+	for (int i = 0; i < N; ++i)
+	{
+		pv[i] = mallocMe(1024*(i+1));
+	}
+	
+	HeapProfilerDump("Try2");
+
+	for (int i = 0; i < N; ++i)
+	{
+		free(pv[i]);
+		pv[i] = 0;
+	}
 
 	return 0;
 }
